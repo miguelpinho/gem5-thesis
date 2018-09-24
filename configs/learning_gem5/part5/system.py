de6773027c49b79f78c1944282914bbad1023cad
miguelp <33704675+miguelpinho@users.noreply.github.com>
2018-09-24 09:36:16 +0100

    tutorial: full system works for x86
    Change-Id: I8a7f9c4e71db65eb8cb73baf9289fc332b15bd90

---
+++ configs/learning_gem5/part5/system.py
@@ -0,0 +1,111 @@
+import m5
+from m5.objects import *
+from m5.util import convert
+
+import x86
+
+from caches import *
+
+class MySystem(LinuxX86System):
+
+    def __init__(self, opts):
+        super(MySystem, self).__init__()
+        self._opts = opts
+
+        self.clk_domain = SrcClockDomain()
+        self.clk_domain.clock = '3GHz'
+        self.clk_domain.voltage_domain = VoltageDomain()
+
+        mem_size = '3GB'
+        self.mem_ranges = [AddrRange(mem_size),
+                           AddrRange(0xC0000000, size=0x100000), # For I/0
+                           ]
+
+        self.membus = SystemXBar()
+        self.membus.badaddr_responder = BadAddr()
+        self.membus.default = self.membus.badaddr_responder.pio
+
+        self.system_port = self.membus.slave
+
+        x86.init_fs(self, self.membus)
+
+        self.kernel = '../full-system/binaries/x86_64-vmlinux-2.6.22.9'
+
+        boot_options = ['earlyprintk=ttys0', 'console=ttyS0',
+                        'lpj=7999923', 'root=/dev/hda1']
+        self.boot_osflags = ' '.join(boot_options)
+
+        self.setDiskImage('../full-system/disks/linux-x86.img')
+
+        self.createCPU()
+
+        self.createCacheHierarchy()
+
+        self.createMemoryControllers()
+
+        self.setupInterrupts()
+
+    def setDiskImage(self, img_path):
+        """ Set the disk image
+            @param img_path path on the host of the image
+                   file for the disk.
+        """
+        disk0 = CowDisk(img_path)
+        self.pc.south_bridge.ide.disks = [disk0]
+
+    def createCPU(self):
+        """ Create a CPU for the system """
+        self.cpu = AtomicSimpleCPU()
+        self.mem_mode = 'atomic'
+        self.cpu.createThreads()
+
+    def createCacheHierarchy(self):
+        """ Create a simple cache hierarchy with the caches form part1 """
+
+        # Create an L1 instruction and data caches and an MMU cache
+        # The MMU cache caches accesses from the inst and data TLBs
+        self.cpu.icache = L1ICache()
+        self.cpu.dcache = L1DCache()
+
+        # Connect the instruction, data, and MMU caches to the CPU
+        self.cpu.icache.connectCPU(self.cpu)
+        self.cpu.dcache.connectCPU(self.cpu)
+
+        # Hook the CPU ports up to the membus
+        self.cpu.icache.connectBus(self.membus)
+        self.cpu.dcache.connectBus(self.membus)
+
+        # Connect the CPU TLBs directly to the mem
+        self.cpu.itb.walker.port = self.membus.slave
+        self.cpu.dtb.walker.port = self.membus.slave
+
+    def createMemoryControllers(self):
+        """ Create the memory controller for the system """
+        self.mem_cntrl = DDR3_1600_8x8(range = self.mem_ranges[0],
+                                       port = self.membus.master)
+
+    def setupInterrupts(self):
+        """ Create the interrupt controller for the CPU """
+        self.cpu.createInterruptController()
+        self.cpu.interrupts[0].pio = self.membus.master
+        self.cpu.interrupts[0].int_master = self.membus.slave
+        self.cpu.interrupts[0].int_slave = self.membus.master
+
+
+class CowDisk(IdeDisk):
+    """ Wrapper class around IdeDisk to make a simple copy-on-write disk
+        for gem5. Creates an IDE disk with a COW read/write disk image.
+        Any data written to the disk in gem5 is saved as a COW layer and
+        thrown away on the simulator exit.
+    """
+
+    def __init__(self, filename):
+        """ Initialize the disk with a path to the image file.
+            @param filename path to the image file to use for the disk.
+        """
+
+        super(CowDisk, self).__init__()
+        self.driveID = 'master'
+        self.image = CowDiskImage(child=RawDiskImage(read_only=True),
+                                  read_only=False)
+        self.image.child.image_file = filename
